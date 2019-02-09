3bc30c569090145adf48ec8096eb3108fae3e30e
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-02-09 00:57:07 +0000

    Se añadió pacakge para propiedades de módulo de prueba

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    hola: function() {
+        console.log('Hola Mundo');
+
+    },
+    holaPersonalizado: function(nombre) {
+        console.log(`Hola ${ nombre }`);
+
+    },
+    holaAlCursoNPM: function() {
+        console.log('Hola al curso NPM !!');
+
+    }
+}
